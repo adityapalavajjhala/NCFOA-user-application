d20a212c666a4af9e38336078bdfc99017c99bf7
Sameera43 <sameeraprathyusha629@gmail.com>
2021-02-27 22:31:24 +0530

    Added Payment_Verification package and related code.

---
+++ app/src/main/java/com/example/ncfoa_user_application/ui/qr_paymentVerification/QrPaymentVerificationFragment.java
@@ -0,0 +1,103 @@
+package com.example.ncfoa_user_application.ui.qr_paymentVerification;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.graphics.Bitmap;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.zxing.WriterException;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+import com.example.ncfoa_user_application.R;
+
+
+public class QrPaymentVerificationFragment extends Fragment {
+    private ImageView qrCodeIV;
+    private EditText dataEdt;
+    private Button generateQrBtn;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
+    public static final String WINDOW_SERVICE = "window";
+
+
+    private QrPaymentVerificationViewModel QrpaymentVerificationViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+       QrpaymentVerificationViewModel =
+                new ViewModelProvider(this).get(QrPaymentVerificationViewModel.class);
+        View qrCodeIV = inflater.inflate(R.layout.fragment_qr_payment_verification, container, false);
+        final ImageView imageView = qrCodeIV.findViewById(R.id.idIVQrcode);
+
+        View dataEdt = inflater.inflate(R.layout.fragment_qr_payment_verification, container, false);
+        final EditText edittextView = qrCodeIV.findViewById(R.id.idEdt);
+
+        View generateQrBtn = inflater.inflate(R.layout.fragment_qr_payment_verification, container, false);
+        final Button buttonview = qrCodeIV.findViewById(R.id.idBtnGenerateQR);
+
+
+        generateQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(edittextView.getText().toString())) {
+
+                    // if the edittext inputs are empty then execute
+                    // this method showing a toast message.
+                    Toast.makeText(getActivity(),"Enter content for QR",Toast.LENGTH_SHORT).show();
+                } else {
+                    // below line is for getting
+                    // the windowmanager service.
+                    DisplayMetrics displaymetrics = new DisplayMetrics();
+                    getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+                    int height = displaymetrics.heightPixels;
+                    int width = displaymetrics.widthPixels;
+                    // generating dimension from width and height.
+                    int dimen = width < height ? width : height;
+                    dimen = dimen * 3 / 4;
+
+                    // setting this dimensions inside our qr code
+                    // encoder to generate our qr code.
+                    qrgEncoder = new QRGEncoder(edittextView.getText().toString(), null, QRGContents.Type.TEXT, dimen);
+                    try {
+                        // getting our qrcode in the form of bitmap.
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        // the bitmap is set inside our image
+                        // view using .setimagebitmap method.
+                        imageView.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        // this method is called for
+                        // exception handling.
+                        Log.e("Tag", e.toString());
+                    }
+                }
+            }
+        });
+
+
+        QrpaymentVerificationViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                edittextView.setText(s);
+            }
+        });
+        return qrCodeIV;
+    }
+
+
+}
+
